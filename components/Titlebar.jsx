911a44de3c28adeb7163e05b857f3acd625131a2
Nitin Ranganath <nitinranganath@gmail.com>
2021-04-26 00:23:29 +0530

    Build titlebar component

---
+++ components/Titlebar.jsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import styles from '../styles/Titlebar.module.css';
+
+const Titlebar = () => {
+  return (
+    <section className={styles.titlebar}>
+      <div className={styles.items}>
+        <Image
+          src="/vscode_icon.svg"
+          alt="VSCode Icon"
+          height={15}
+          width={15}
+        />
+        <p>File</p>
+        <p>Edit</p>
+        <p>View</p>
+        <p>Go</p>
+        <p>Run</p>
+        <p>Terminal</p>
+        <p>Help</p>
+      </div>
+      <p className={styles.title}>Nitin Ranganath - Visual Studio Code</p>
+      <div className={styles.windowButtons}>
+        <span className={styles.minimize}></span>
+        <span className={styles.maximize}></span>
+        <span className={styles.close}></span>
+      </div>
+    </section>
+  );
+};
+
+export default Titlebar;
