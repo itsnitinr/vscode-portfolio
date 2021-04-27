4efed7e756373436fba1a35240bcff899eaa562d
Nitin Ranganath <nitinranganath@gmail.com>
2021-04-27 13:43:26 +0530

    Initial home page design

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,24 @@
+import Link from 'next/link';
 import Head from '../components/Head';
+import styles from '../styles/HomePage.module.css';
 
-export default function Home() {
+export default function HomePage() {
   return (
     <>
       <Head title="Nitin Ranganath | Home" />
-      <h1>Home Page</h1>
+      <div className={styles.container}>
+        <h6 className={styles.welcome}>Hey there, geeks! I am</h6>
+        <h1 className={styles.title}>Nitin Ranganath</h1>
+        <h3 className={styles.subtitle}>&lt;!-- Web Developer --&gt;</h3>
+        <div className={styles.buttons}>
+          <Link href="/about">
+            <button className={styles.contained}>What Can I Do?</button>
+          </Link>
+          <Link href="/contact">
+            <button className={styles.outlined}>React Out to Me</button>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
