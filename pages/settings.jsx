5f294dcb581d03faf5ee2a332f2b562293e4cb65
Nitin Ranganath <nitinranganath@gmail.com>
2021-05-05 22:30:18 +0530

    Add Ayu theme

--- pages/settings.jsx
+++ pages/settings.jsx
@@ -22,6 +22,20 @@ const SettingsPage = () => {
           theme="dracula"
           description="Official Dracula Theme. A dark theme for many editors, shells, and more."
         />
+        <ThemeInfo
+          name="Ayu Dark"
+          icon="/ayu.png"
+          publisher="teabyii"
+          theme="ayu-dark"
+          description="A simple theme with bright colors."
+        />
+        <ThemeInfo
+          name="Ayu Mirage"
+          icon="/ayu.png"
+          publisher="teabyii"
+          theme="ayu-mirage"
+          description="A simple theme with bright colors."
+        />
       </div>
     </>
   );
