6858fb87348c38cf51bcaf8e319114585548b7f0
Nitin Ranganath <nitinranganath@gmail.com>
2021-06-02 20:14:39 +0530

    Initial GitHub page design

---
+++ components/RepoCard.jsx
@@ -0,0 +1,42 @@
+import WatchIcon from '../components/icons/WatchIcon';
+import ForkIcon from '../components/icons/ForkIcon';
+import StarIcon from '../components/icons/StarIcon';
+import GithubIcon from '../components/icons/GithubIcon';
+import LinkIcon from '../components/icons/LinkIcon';
+import styles from '../styles/RepoCard.module.css';
+
+const RepoCard = ({ repo }) => {
+  return (
+    <div className={styles.card}>
+      <div>
+        <h3 className={styles.title}>{repo.name}</h3>
+        <p>{repo.description}</p>
+      </div>
+      <div className={styles.stats}>
+        <div>
+          <div>
+            <WatchIcon className={styles.icon} /> {repo.watchers}
+          </div>
+          <div>
+            <ForkIcon className={styles.icon} /> {repo.forks}
+          </div>
+          <div>
+            <StarIcon className={styles.icon} /> {repo.stargazers_count}
+          </div>
+        </div>
+        <div>
+          <a href={repo.html_url} target="_blank" rel="noopener">
+            <GithubIcon height={20} width={20} className={styles.icon} />
+          </a>
+          {repo.homepage && (
+            <a href={repo.homepage} target="_blank" rel="noopener">
+              <LinkIcon height={20} width={20} className={styles.icon} />
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RepoCard;
--- pages/github.jsx
+++ pages/github.jsx
@@ -1,14 +1,62 @@
-const GithubPage = () => {
+import Image from 'next/image';
+import GitHubCalendar from 'react-github-calendar';
+import RepoCard from '../components/RepoCard';
+import styles from '../styles/GithubPage.module.css';
+
+const GithubPage = ({ repos, user }) => {
+  const contributionsTheme = {
+    text: '#eee',
+  };
+
   return (
     <>
-      <h1>Github Page</h1>
+      <div className={styles.user}>
+        <div>
+          <Image
+            src={user.avatar_url}
+            className={styles.avatar}
+            alt={user.login}
+            width={50}
+            height={50}
+          />
+          <h3 className={styles.username}>{user.login}</h3>
+        </div>
+        <div>
+          <h3>{user.public_repos} repos</h3>
+        </div>
+        <div>
+          <h3>{user.followers} followers</h3>
+        </div>
+      </div>
+      <h2>6 Latest Updated Repositories</h2>
+      <div className={styles.container}>
+        {repos.map((repo) => (
+          <RepoCard key={repo.id} repo={repo} />
+        ))}
+      </div>
+      <GitHubCalendar
+        username={process.env.NEXT_PUBLIC_GITHUB_USERNAME}
+        theme={contributionsTheme}
+        blockMargin={2}
+        blockSize={20}
+      />
     </>
   );
 };
 
 export async function getStaticProps() {
+  const userRes = await fetch(
+    `https://api.github.com/users/${process.env.NEXT_PUBLIC_GITHUB_USERNAME}`
+  );
+  const user = await userRes.json();
+
+  const repoRes = await fetch(
+    `https://api.github.com/users/${process.env.NEXT_PUBLIC_GITHUB_USERNAME}/repos?sort=created_at&per_page=6`
+  );
+  const repos = await repoRes.json();
+
   return {
-    props: { title: "Github" },
+    props: { title: 'GitHub', repos, user, revalidate: 60 },
   };
 }
 
