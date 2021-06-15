efc4a1ec8f6e6a439f48c9853347eefc9ab61bb9
Nitin Ranganath <nitinranganath@gmail.com>
2021-06-15 10:20:23 +0530

    Add OpenGraph and Twitter meta tags

--- components/Head.jsx
+++ components/Head.jsx
@@ -12,6 +12,14 @@ const CustomHead = ({ title }) => {
         name="keywords"
         content="nitin, nitin ranganath, ranganath, web developer, nitin web developer, nitin developer, mern stack, nitin portfolio"
       />
+      <meta property="og:title" content="Nitin Ranganath's Portfolio" />
+      <meta
+        property="og:description"
+        content="A full-stack developer building websites that you'd like to use."
+      />
+      <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
+      <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
+      <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
 };
