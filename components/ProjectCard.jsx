a799e62a917248c63bcc46fcb742886d969ef117
Nitin Ranganath <nitinranganath@gmail.com>
2021-08-04 15:22:56 +0530

    Projects page improvements

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -8,6 +8,33 @@ const ProjectCard = ({ project }) => {
       <div className={styles.content}>
         <h3>{project.name}</h3>
         <p>{project.description}</p>
+        <div className={styles.tags}>
+          {project.tags.map((tag) => (
+            <span key={tag} className={tag}>
+              {tag}
+            </span>
+          ))}
+        </div>
+        <div className={styles.cta}>
+          {project.source_code && (
+            <a
+              href={project.source_code}
+              target="_blank"
+              rel="noopener"
+              className={styles.underline}
+            >
+              Source Code
+            </a>
+          )}
+          <a
+            href={project.demo}
+            target="_blank"
+            rel="noopener"
+            className={styles.underline}
+          >
+            Live Demo
+          </a>
+        </div>
       </div>
     </div>
   );
