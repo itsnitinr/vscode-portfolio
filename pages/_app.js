3ec14578da8778be07ea258deec1122d7170e9d6
Daniel Custodio <play.daniel@hotmail.com>
2022-09-25 18:41:26 -0300

    feat: Home page changes

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <Layout>
-      <Head title={`Nitin Ranganath | ${pageProps.title}`} />
+      <Head title={`Daniel Custódio| ${pageProps.title}`} />
       <Component {...pageProps} />
     </Layout>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,27 +1,22 @@
 import Link from 'next/link';
-import Illustration from '../components/Illustration';
 import styles from '../styles/HomePage.module.css';
 
 export default function HomePage() {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.background}>
-          <h1>I BUILD</h1>
-          <h1>WEBSITES</h1>
-        </div>
         <div className={styles.foreground}>
           <div className={styles.content}>
-            <h1 className={styles.name}>Nitin Ranganath</h1>
+            <h1 className={styles.name}>Daniel Custódio</h1>
             <h6 className={styles.bio}>Full Stack Web Developer</h6>
             <Link href="/projects">
-              <button className={styles.button}>View Work</button>
+              <button className={styles.button}>Veja meus projeto</button>
             </Link>
             <Link href="/contact">
-              <button className={styles.outlined}>Contact Me</button>
+              <button className={styles.outlined}>Contato</button>
             </Link>
           </div>
-          <Illustration className={styles.illustration} />
+          <img src="background.svg"  className={styles.illustration} alt="" />
         </div>
       </div>
     </>
