cc53d04fec6f5b8fe85b49599518563003828b25
Daniel Custodio <play.daniel@hotmail.com>
2022-09-26 16:28:52 -0300

    feat: Update SEO

--- components/Head.jsx
+++ components/Head.jsx
@@ -18,7 +18,7 @@ const CustomHead = ({ title }) => {
         content="Desenvolvedor fullstack, construo sistemas e sites web."
       />
       <meta property="og:image" content="https://imgur.com/g6acT2j.png" />
-      <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
+      <meta property="og:url" content="https://portfolio-daniel-custodio.vercel.app/" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
