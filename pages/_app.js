088d0cffe3fcae8d1eb8f689f965265eac8affc9
Daniel Custodio <play.daniel@hotmail.com>
2022-09-28 11:13:45 -0300

    chore: Google Analytics Tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Script from 'next/script';
 import Layout from "../components/Layout";
 import Head from "../components/Head";
 import "../styles/globals.css";
@@ -17,6 +18,18 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <Layout>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+      <Script strategy="lazyOnload">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+              });
+          `}
+      </Script>
       <Head title={`Daniel Custódio | ${pageProps.title}`} />
       <Component {...pageProps} />
     </Layout>
