79622c891b08fe0182c8d59e7e012c44b535205a
Daniel Custodio <play.daniel@hotmail.com>
2022-10-20 12:44:40 -0300

    refactor: Curriculum download button

--- components/ButtonCV.jsx
+++ components/ButtonCV.jsx
@@ -1,11 +1,11 @@
 import styles from '../styles/ButtonCV.module.css'
 const ButtonCV = () => {
   return(
-    <button className={styles.container}>
-      <a href="https://docs.google.com/uc?export=download&id=1Fvf6N4Mxz_egxrGLAWyffZfr1OEUny6d" download>
-        Baixar Currículo 
-    </a>
-    <img src="/download.png" alt="Botão para baixar curriculo" />
+      <button className={styles.container}>
+        <a href="https://docs.google.com/uc?export=download&id=1Fvf6N4Mxz_egxrGLAWyffZfr1OEUny6d" download>
+          Baixar Currículo
+          <img src="/download.png" alt="Botão para baixar curriculo" />
+        </a>
     </button>
   )
 }
