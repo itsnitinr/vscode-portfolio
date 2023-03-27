15010fc27f0776f05f739b71a40fa3464b1d8bf6
Daniel Custodio <play.daniel@hotmail.com>
2023-03-27 16:06:40 -0300

    feat: Adding project: Accounts

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -6,6 +6,28 @@ const ProjectCard = ( { project } ) => {
     <div className={ styles.card }>
       <Image src={ project.image } height={ 300 } width={ 600 } alt={ project.name } />
       <div className={ styles.content }>
+        <div className={styles.cta}>
+          {project.source_code&&(
+            <a
+              href={project.source_code}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.underline}
+            >
+              Repositório
+            </a>
+          )}
+          {project.demo&&(
+            <a
+              href={project.demo}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.underline}
+            >
+              Demonstração Online
+            </a>
+          )}
+        </div>
         <h3>{ project.name }</h3>
         <p>{ project.description }</p>
         <div className={ styles.tags }>
@@ -14,28 +36,6 @@ const ProjectCard = ( { project } ) => {
               { tag }
             </span>
           ) ) }
-        </div>
-        <div className={ styles.cta }>
-          { project.source_code && (
-            <a
-              href={ project.source_code }
-              target="_blank"
-              rel="noopener noreferrer"
-              className={ styles.underline }
-            >
-              Repositório
-            </a>
-          ) }
-          { project.demo && (
-            <a
-              href={ project.demo }
-              target="_blank"
-              rel="noopener noreferrer"
-              className={ styles.underline }
-            >
-              Demonstração Online
-            </a>
-          ) }
         </div>
       </div>
     </div>
