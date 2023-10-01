98980583695bdce53257ac224046846a913cc6c2
Daniel Custódio <play.daniel@hotmail.com>
2023-10-01 16:54:41 -0300

    feat: Update curriculum

--- components/ButtonCV.jsx
+++ components/ButtonCV.jsx
@@ -2,7 +2,7 @@ import styles from '../styles/ButtonCV.module.css'
 const ButtonCV = () => {
   return(
       <button className={styles.container}>
-        <a href="https://docs.google.com/uc?export=download&id=1Fvf6N4Mxz_egxrGLAWyffZfr1OEUny6d" download>
+      <a href="https://docs.google.com/uc?export=download&id=1TaKBf3J88BnNLmug-OIQ_TF9FCWgp0hD" download>
           Baixar Currículo
           <img src="/download.png" alt="Botão para baixar curriculo" />
         </a>
