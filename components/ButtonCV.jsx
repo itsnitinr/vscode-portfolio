52d8b310ee714142f5118ceec4fe5cda337be72a
Daniel Custódio <play.daniel@hotmail.com>
2024-07-09 14:06:31 -0300

    feat: Update curriculum

--- components/ButtonCV.jsx
+++ components/ButtonCV.jsx
@@ -2,7 +2,7 @@ import styles from '../styles/ButtonCV.module.css'
 const ButtonCV = () => {
   return(
       <button className={styles.container}>
-      <a href="https://docs.google.com/uc?export=download&id=1IHNjQttFWvgBYgbfZViqPnIWDXjqlZoZ" download>
+      <a href="https://docs.google.com/uc?export=download&id=13Q96f1UOQfAskwA9C0Ykba_sWeqdOc4Q" download>
           Baixar Currículo
           <img src="/download.png" alt="Botão para baixar curriculo" />
         </a>  
