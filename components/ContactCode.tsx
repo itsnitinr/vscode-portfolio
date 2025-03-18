2d4efe20299e3ee9cd3724d5b7f92635bb3e835b
Nitin Ranganath <nitinranganath@gmail.com>
2025-03-18 23:35:13 +0530

    improved contact page

--- components/ContactCode.tsx
+++ components/ContactCode.tsx
@@ -3,8 +3,8 @@ import styles from '@/styles/ContactCode.module.css';
 const contactItems = [
   {
     social: 'website',
-    link: 'nitinranganath.me',
-    href: 'https://nitinranganath.me',
+    link: 'nitinranganath.com',
+    href: 'https://nitinranganath.com',
   },
   {
     social: 'email',
@@ -27,29 +27,14 @@ const contactItems = [
     href: 'https://www.twitter.com/iamnitinr',
   },
   {
-    social: 'instagram',
-    link: 'iamnitinr',
-    href: 'https://www.instagram.com/iamnitinr',
-  },
-  {
-    social: 'polywork',
-    link: 'nitinranganath',
-    href: 'https://www.polywork.com/nitinranganath',
-  },
-  {
     social: 'telegram',
     link: 'iamnitinr',
     href: 'https://t.me/iamnitinr',
   },
   {
-    social: 'codepen',
+    social: 'peerlist',
     link: 'nitinranganath',
-    href: 'https://codepen.io/itsnitinr',
-  },
-  {
-    social: 'codesandbox',
-    link: 'itsnitinr',
-    href: 'https://codesandbox.io/u/itsnitinr',
+    href: 'https://peerlist.io/nitinranganath',
   },
 ];
 
@@ -59,18 +44,9 @@ const ContactCode = () => {
       <p className={styles.line}>
         <span className={styles.className}>.socials</span> &#123;
       </p>
-      {contactItems.slice(0, 8).map((item, index) => (
+      {contactItems.map((item, index) => (
         <p className={styles.line} key={index}>
           &nbsp;&nbsp;&nbsp;{item.social}:{' '}
-          <a href={item.href} target="_blank" rel="noopener">
-            {item.link}
-          </a>
-          ;
-        </p>
-      ))}
-      {contactItems.slice(8, contactItems.length).map((item, index) => (
-        <p className={styles.line} key={index}>
-          &nbsp;&nbsp;{item.social}:{' '}
           <a href={item.href} target="_blank" rel="noopener">
             {item.link}
           </a>
