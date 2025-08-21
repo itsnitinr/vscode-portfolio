cb2f18ca928b9b92fabf9bf8b10941d90e30e76e
John Abascal <abascal.j@northeastern.edu>
2025-08-21 01:15:48 -0400

    Update ContactCode.tsx

--- components/ContactCode.tsx
+++ components/ContactCode.tsx
@@ -2,41 +2,41 @@ import styles from '@/styles/ContactCode.module.css';
 
 const contactItems = [
   {
-    social: 'email',
+    platform: 'email',
     link: 'abascal.j@northeastern.edu',
     href: 'mailto:abascal.j@northeastern.edu',
   },
   {
-    social: 'github',
-    link: 'johnmath',
-    href: 'https://github.com/johnmath',
-  },
-  {
-    social: 'linkedin',
+    platform: 'linkedin',
     link: 'johnabascal',
     href: 'https://www.linkedin.com/in/johnabascal/',
   },
   {
-    social: 'scholar',
+    platform: 'github',
+    link: 'johnmath',
+    href: 'https://github.com/johnmath',
+  },
+  {
+    platform: 'scholar',
     link: '3jYQ3FsAAAAJ&hl',
     href: 'https://scholar.google.com/citations?user=3jYQ3FsAAAAJ&hl',
   },
-  {
-    social: 'website',
-    link: 'johnmath.github.io',
-    href: 'https://johnmath.github.io',
-  },
+  // {
+  //   platform: 'website',
+  //   link: 'johnmath.github.io',
+  //   href: 'https://johnmath.github.io',
+  // },
 ];
 
 const ContactCode = () => {
   return (
     <div className={styles.code}>
       <p className={styles.line}>
-        <span className={styles.className}>.socials</span> &#123;
+        <span className={styles.className}>.contact</span> &#123;
       </p>
       {contactItems.map((item, index) => (
         <p className={styles.line} key={index}>
-          &nbsp;&nbsp;&nbsp;{item.social}:{' '}
+          &nbsp;&nbsp;&nbsp;{item.platform}:{' '}
           <a href={item.href} target="_blank" rel="noopener">
             {item.link}
           </a>
